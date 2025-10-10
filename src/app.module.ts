326b485bab20ae7cdecf211326e5d281b326b46e
shariyerShazan <shariyershazan1@gmail.com>
2025-10-11 00:30:11 +0600

    -

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,18 +5,16 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { StudentModule } from './student/student.module';
-import { ProductService } from './product/product.service';
-import { ProductController } from './product/product.controller';
-import { ProductModule } from './product/product.module';
+
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.DATABASE_URL!),
     StudentModule,
-    ProductModule
+
   ],
-  controllers: [AppController, ProductController],
-  providers: [AppService, ProductService],
+  controllers: [AppController, ],
+  providers: [AppService, ],
 })
 export class AppModule {}
